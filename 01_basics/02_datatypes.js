dfbfe0a8bd91d79808177e803bad6f342fc44414
pralabh7 <pralabhmishra343@gmail.com>
2025-05-19 08:54:54 +0000

    datatype info

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert(3+3) //can't coz we are using nodejs, not browser
+
+console.log(3
+    +
+    3   //code readability should be high
+)
+let name="Pralabh"
+let age=20
+let isLoggedIn=true
+let bloodgrp
+let temp=null
+
+// Primitive
+// number => 2 to the power 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// null => Stanalone value
+// undefined => Value not defined yet
+// symbol => unique
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof "Pralabh")
+console.log(typeof bloodgrp)/// gives undefined
+console.log(typeof null) // gives object
+console.log(typeof null)// gives object
