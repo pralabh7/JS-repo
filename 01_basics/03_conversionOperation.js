1550847cf044fb6399a2b5273d45dcda07086490
pralabh7 <pralabhmishra343@gmail.com>
2025-05-19 10:01:51 +0000

    conversion info

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score=true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let convetToNumber=Number(score)
+// console.log(typeof convetToNumber)
+// console.log(convetToNumber)
+
+// "33" => 33
+// "33anb" => NaN
+// true => 1
+// null => 0
+// undefined => NaN
+
+let isLoggedIn= 12
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//"" => false
+//"Pralabh" => true
+// 20 => true
+// 0 => false
+
+let num=true
+
+let stringNum= String(num)
+// console.log(typeof stringNum)
+// console.log(num)
+
+let value=3
+let negativeval=-value
+// console.log(-negativeval)
+
+// console.log(2*9)
+// console.log(2**4)
+// console.log(2/3)
+// console.log(7%3)
+
+let str1="Pralabh"
+let str2=" Mishra"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log(1+2)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log("a"+2+3)
+// console.log(1+2+"a")
+
+//console.log(3+4*4/3%3) DON'T USE SUCH CODES
+
+// console.log(+true) Phaaltu codes but executable-don't prefer them
+// console.log(+"") same thing as above
+
+let num1, num2, num3
+num1=num2=num3=2+2
+// console.log(num1,num2,num3) such codes work but reduces readability
+
+let marks=100;
+// let pre=++marks
+// console.log(marks) updated older than alloted older
+// console.log(pre)
+
+// let post=marks++
+// console.log(marks) alloted older than updated older
+// console.log(post)
